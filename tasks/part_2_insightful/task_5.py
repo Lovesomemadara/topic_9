297ebf5ea3590b0a9d867bb8b3ccd9fe5ddcc1d3
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-01-26 10:09:37 +0000

    Проверка ДЗ |  9. Работа со строками [Познаватель]
    - Правильно:
    - Неправильно:
    - Улучшены:
    - На доработку:
    
    Все задания решены!

--- tasks/part_2_insightful/task_5.py
+++ tasks/part_2_insightful/task_5.py
@@ -1,6 +1,8 @@
 line: str = input().strip()
+
 vowels: str = 'aeiouAEIOUаоуыэеёиюяАОУЫЭЕЁИЮЯ'
 replace: str = '?' * 30
+
 correct_line: dict[int, str | None] = line.maketrans(vowels, replace)
 
 print(line.translate(correct_line))
